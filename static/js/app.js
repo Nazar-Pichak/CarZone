65111fbb8982f3b7182b84df90bd139b86f7227d
pichak_NAZARIY1996 <pichak.nazariy@gmail.com>
2023-10-28 23:28:07 +0200

    fixing bugs

--- static/js/app.js
+++ static/js/app.js
@@ -108,19 +108,19 @@ $(function () {
                     $('.sticky-header').addClass('header-shrink');
                 }
                 if($('.do-sticky').length < 1) {
-                    $('.company-logo img').attr('src', 'img/logos/black-logo.png');
+                    $('.company-logo img').attr('src', '../static/img/logos/black-logo.png');
                 }
             }
             else {
                 $('.sticky-header').removeClass('header-shrink');
                 if($('.do-sticky').length < 1 && $('.fixed-header').length == 0 && $('.fixed-header2').length == 0) {
-                    $('.company-logo img').attr('src', 'img/logos/logo.png');
+                    $('.company-logo img').attr('src', '../static/img/logos/logo.png');
                 } else {
-                    $('.company-logo img').attr('src', 'img/logos/black-logo.png');
+                    $('.company-logo img').attr('src', '../static/img/logos/black-logo.png');
                 }
             }
         } else {
-            $('.company-logo img').attr('src', 'img/logos/black-logo.png');
+            $('.company-logo img').attr('src', '../static/img/logos/black-logo.png');
         }
     }
 
@@ -128,7 +128,7 @@ $(function () {
     {
         if ($(document).scrollTop() > 40) {
             $('.do-sticky').addClass('sticky-header');
-            //$('.do-sticky').addClass('header-shrink');
+            // $('.do-sticky').addClass('header-shrink');
         }
         else {
             $('.do-sticky').removeClass('sticky-header');
