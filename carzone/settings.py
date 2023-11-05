a91360c16e815318c4399ff3a199fd934c5223cc
pichak_NAZARIY1996 <pichak.nazariy@gmail.com>
2023-11-05 11:12:17 +0100

    set dynamic titles for all pages

--- carzone/settings.py
+++ carzone/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'carzone_db',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
